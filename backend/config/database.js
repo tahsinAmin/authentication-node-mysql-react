b4960e57112f68ec6e3b812ac84c0e4c27f191d5
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-01-05 19:13:41 +0600

    Database Sync

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', {
+const db = new Sequelize('auth_db', 'root', '', {
     host: 'localhost',
-    dialact: "mysql",
+    dialect: 'mysql',
 });
 
 export default db;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import session from "express-session";
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
+import db from "./config/database.js"
+import UserRoute from "./routes/userRoute.js"
+import ProductRoute from "./routes/productRoute.js"
 dotenv.config();
 
 const app = express();
+
+(async() => {
+    await db.sync();
+})();
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -18,9 +24,12 @@ app.use(session({
 
 app.use(cors({
     credentials: true,
-    origin: 'http://local'
+    origin: 'http://localhost:3000'
 }))
+
 app.use(express.json())
+app.use(UserRoute)
+app.use(ProductRoute)
 
 app.listen(process.env.APP_PORT, () => {
     console.log("Server is running...");
