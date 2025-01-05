fa389a7c2e08eeca2bef1746a4512e35685f71b0
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-01-06 00:15:39 +0600

    Connect Session Sequelize

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,20 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";  
-// import db from "./config/database.js"
+import db from "./config/database.js"
+import SequelizeStore from "connect-session-sequelize"
 import UserRoute from "./routes/userRoute.js"
 import ProductRoute from "./routes/productRoute.js"
 import AuthRoute from "./routes/authRoute.js"
 dotenv.config();
 
 const app = express();
+
+const sessionStore =  SequelizeStore(session.Store);
+
+const store  = new sessionStore({
+    db: db
+})
 
 // Commenting this because database has been created 
 // (async() => {
@@ -19,6 +26,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -33,6 +41,8 @@ app.use(express.json())
 app.use(UserRoute)
 app.use(AuthRoute)
 app.use(ProductRoute)
+
+// store.sync()
 
 app.listen(process.env.APP_PORT, () => {
     console.log("Server is running...");
