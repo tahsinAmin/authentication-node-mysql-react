fc7ec27734421e789142771705bb902d788eddb1
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-01-08 08:29:01 +0600

    Edit Product

--- frontend/src/components/FormEditProduct.jsx
+++ frontend/src/components/FormEditProduct.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const FormEditProduct = () => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("")
+    const [msg, setMsg] = useState("")
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    useEffect(() => {
+        const getProductById = async() => {
+            try {
+                const response = await axios.get(`http://localhost:5000/products/${id}`);
+                setName(response.data.name);
+                setPrice(response.data.price);
+            } catch (error) { 
+                if (error.response) {
+                    setMsg(error.response.data.msg)
+                }
+            }
+        }
+        getProductById();
+    }, [id])
+
+    const updateProduct = async(e) => {
+        e.preventDefault();
+
+        try {
+            await axios.patch(`http://localhost:5000/products/${id}`, {
+                name: name,
+                price:price
+            })
+            navigate("/products")
+        } catch (error) {
+            if (error.response) {
+                setMsg(error.response.data.msg)
+            }
+        }
+    }
   return (
     <div>
         <h1 className='title'>Products</h1>
@@ -8,27 +46,28 @@ export const FormEditProduct = () => {
         <div className="card is-shadowless">
             <div className="card-content">
                 <div className="content">
-                <form>
-                <div className="field">
-                        <label className='label'>Product Name</label>
-                        <div className="control">
-                            <input type="text" className="input" placeholder='Product Name'/>
+                    <form onSubmit={updateProduct}>
+                        <p className="has-text-centered">{msg}</p>
+                        <div className="field">
+                            <label className='label'>Product Name</label>
+                            <div className="control">
+                                <input value={name} onChange={(e) => setName(e.target.value)} type="text" className="input" placeholder='Product Name'/>
+                            </div>
                         </div>
-                    </div>
-                    <div className="field">
-                        <label className='label'>Price</label>
-                        <div className="control">
-                            <input type="text" className="input" placeholder='Price'/>
+                        <div className="field">
+                            <label className='label'>Price</label>
+                            <div className="control">
+                                <input value={price} onChange={(e) => setPrice(e.target.value)} type="text" className="input" placeholder='Price'/>
+                            </div>
                         </div>
-                    </div>
+                        
                     
-                   
-                    <div className="field">
-                        <div className="control">
-                        <button className='button is-success'>Update</button>
+                        <div className="field">
+                            <div className="control">
+                            <button type='submit' className='button is-success'>Update</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
                 </div>
             </div>
         </div>
